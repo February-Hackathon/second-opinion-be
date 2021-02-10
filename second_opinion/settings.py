1f1094850900e79d735d4d9785fcc6375c11c839
AshveerB <ashveerbhayroo@gmail.com>
2021-02-09 18:37:20 -0800

    deploy

--- second_opinion/settings.py
+++ second_opinion/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -154,3 +155,5 @@ STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'users.User'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
+django_heroku.settings(locals())
